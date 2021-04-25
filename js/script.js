6dfc271d2b4f415574b44a489f27694cd3caccf1
Omar Alazaizeh <omx302@gmail.com>
2021-04-25 13:37:26 +0300

    1st update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let name = prompt("Enter your name please");
+alert("Welcome ", name);
+let age = parseInt(prompt("Enter your age please"));
+alert("WOW !!  ", 2020 - age);
+let nationality = prompt("Enter your nationality please");
+let education = prompt("Enter your name highest education please");
+
